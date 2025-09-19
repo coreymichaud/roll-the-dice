a31824dd2c7595fa3cfd535ebaeb4bf8d7c54dd5
Corey M. <cor3y.michaud@gmail.com>
2025-09-19 07:01:12 -0400

    Updated home page text to include motivation.

--- views/general/home.py
+++ views/general/home.py
@@ -18,10 +18,15 @@ with col2:
 
 st.markdown("""
             
-# A Little About Me
+# Motivation
+
+There are a lot of formulas when it comes to statistics, and it's hard to try to memorize them all. In college, statistics is *clearly* the best math subject, so it was the only one that lets you use a calculator or code to solve problems.
+Graphing calculators have a lot of functionality, but they don't have everything. That's usually where code comes in, but then you have to remember how to do each test. What if you know how to do a 2-sample Z-interval in R, but
+your situation only allows for Python? That's *hopefully* where this site comes in.
             
-Is this back to normal? SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT 
-SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT 
+This site is meant to not only be a calculator for statistics, but a way to learn how to use these formulas. Hopefully, you are able to learn something from using each page.
+            
+Also, this is for my own personal learning. It would make sense that if I graduated college years ago and never use these formulas, that I would forget them if they ever came up again. This is my way of making sure I don't forget.
 
 # How It's Made
 
